17ce4ad777585bf662ef92551e47b03f71a15760
dev-aly3n <dev.aly3n@gmail.com>
2023-03-18 19:42:26 +0330

    install pkgs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/**
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+
+  async redirects() {
+    return [
+      
+    ];
+  },
+  
+};
+
+module.exports = nextConfig;
---
+++ src/assets/font.ts
@@ -0,0 +1,10 @@
+import { Outfit } from "next/font/google";
+
+export const outfit = Outfit({
+  subsets: ["latin"],
+  // weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  // style: ["normal"],
+  // variable: "--font-outfit",
+  // preload: true,
+  // display: "block",
+});
