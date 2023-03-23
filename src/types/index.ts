e7cae5d59ac60f3b99b7b64017e14a8915ddebe6
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2023-03-23 22:18:12 +0330

    types(feat): add pool type

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Pool {
+    underlyingLabel: string;
+    underlyingIcon: JSX.Element;
+    accountingLabel: string;
+    accountingIcon: JSX.Element;
+    value: string;
+}
+
+export interface PoolState {
+    pool: string;
+    updatePool: (newPool: string) => void
+}
