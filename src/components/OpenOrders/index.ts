8cbfe1287a611473ad4091b13275ed64ca0c47c3
dev-aly3n <dev.aly3n@gmail.com>
2023-03-24 17:47:22 +0330

    fix import

--- src/components/OpenOrders/index.ts
+++ src/components/OpenOrders/index.ts
@@ -1,1 +1,1 @@
-export { default } from './OpenOrders';
+export {OpenOrders} from './OpenOrders';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,19 @@
 export interface Pool {
-    underlyingLabel: string;
-    underlyingIcon: JSX.Element;
-    accountingLabel: string;
-    accountingIcon: JSX.Element;
-    value: string;
+  underlyingLabel: string;
+  underlyingIcon: JSX.Element;
+  accountingLabel: string;
+  accountingIcon: JSX.Element;
+  value: string;
 }
 
 export interface PoolState {
-    pool: string;
-    updatePool: (newPool: string) => void
+  pool: string;
+  updatePool: (newPool: string) => void;
 }
 
 export interface orderType {
-    id:string;
-    value:number;
-    volume:number;
-    time:number;
+  id: string;
+  value: number;
+  volume: number;
+  time: number;
 }
