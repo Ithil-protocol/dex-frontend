464e7545fe0354b89e5a4222ec44171c2513437d
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-27 03:21:12 +0330

    MarketTrades(feat): update MarketTrades

--- src/components/MarketTrades/MarketTrades.tsx
+++ src/components/MarketTrades/MarketTrades.tsx
@@ -1,7 +1,25 @@
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import LightTooltip from "components/Utility/LightTooltip";
+import Trades from "./Trades";
+
 const MarketTrades = () => {
   return (
-    <div>
-      <h2>Market Trades</h2>
+    <div style={{ backgroundColor: "#233347" }}>
+      <div
+        style={{
+          display: "flex",
+          gap: 5,
+          padding: 10,
+          alignItems: "center",
+        }}
+      >
+        <h4 style={{ color: "#DFE6EC" }}>Market Trades</h4>
+        <LightTooltip title="All the trades happening">
+          <InfoOutlinedIcon style={{ color: "white" }} fontSize="small" />
+        </LightTooltip>
+      </div>
+
+      <Trades />
     </div>
   );
 };
