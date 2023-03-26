02f7829faf0f93e413672d1e607bf4a62d4acb05
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-27 03:20:22 +0330

    Trades(feat): add Trades component

---
+++ src/components/MarketTrades/Trades/Trades.tsx
@@ -0,0 +1,60 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Trade from "components/MarketTrades/Trade/Trade";
+import * as React from "react";
+import { buyOrders } from "store/web3Store";
+
+export default function Trades() {
+  return (
+    <TableContainer
+      component={(props) => (
+        <Paper {...props} style={{ backgroundColor: "#233347" }} />
+      )}
+    >
+      <Table sx={{}} size="small" aria-label="a dense table">
+        <TradeTableHead />
+        <TradesTableBody />
+      </Table>
+    </TableContainer>
+  );
+}
+
+const TradeTableHead = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        {["value", "volume", "time"].map((item, i) => (
+          <TableCell
+            key={i}
+            style={{
+              color: "white",
+            }}
+            align="left"
+          >
+            {item}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+const TradesTableBody = () => {
+  const orders = React.useMemo(
+    () => buyOrders.sort((a, b) => b.time - a.time).slice(10),
+    []
+  );
+
+  return (
+    <TableBody>
+      {orders.map((item, i) => (
+        <Trade key={i} item={item} />
+      ))}
+    </TableBody>
+  );
+};
