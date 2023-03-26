8635a76cec4854656e0fc6ae313bc92161192125
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2023-03-27 00:47:53 +0330

    1

--- src/components/Orders/Order/Order.tsx
+++ src/components/Orders/Order/Order.tsx
@@ -1,15 +1,19 @@
 import styles from "./Order.module.scss";
 import { Order as OrderType } from "types";
 
-const Order = ({ value, volume }: OrderType) => {
+interface Props extends OrderType {
+  type: "buy" | "sell";
+}
+
+const Order = ({ value, volume, type }: Props) => {
   const base = 20;
 
   const width = (Math.min(volume, base) / base) * 100 + "%";
 
   return (
-    <div className={styles.order}>
+    <div className={styles.order} data-type={type}>
       <div className={styles.background} style={{ width }}></div>
-      <p>{value}</p>
+      <p className={styles.value}>{value}</p>
       <p>{volume}</p>
     </div>
   );
--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,11 +1,16 @@
+import styles from "./Orders.module.scss";
 import Order from "./Order";
-import { buyOrders } from "store/web3Store";
+import { computedOrders } from "store/web3Store";
 
 const Orders = () => {
   return (
     <div>
-      {buyOrders.map((order) => (
-        <Order {...order} key={order.id} />
+      {computedOrders.slice(-8).map((order) => (
+        <Order {...order} key={order.id + "buy"} type="buy" />
+      ))}
+      <p className={styles.last}>9854.236</p>
+      {computedOrders.slice(-8).map((order) => (
+        <Order {...order} key={order.id + "sell"} type="sell" />
       ))}
     </div>
   );
