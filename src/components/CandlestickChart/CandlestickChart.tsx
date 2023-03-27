d6e700c3338d4a999eac239f1dbb55e32dcb4b33
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2023-03-27 20:39:04 +0330

    add dynamic pool support for candlestick chart

--- src/components/CandlestickChart/CandlestickChart.tsx
+++ src/components/CandlestickChart/CandlestickChart.tsx
@@ -1,4 +1,6 @@
+import { pools } from "data/pools";
 import dynamic from "next/dynamic";
+import { usePoolStore } from "store";
 const SymbolOverviewNoSSR = dynamic(
   () =>
     import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
@@ -8,5 +10,19 @@ const SymbolOverviewNoSSR = dynamic(
 );
 
 export const CandlestickChart = () => {
-  return <SymbolOverviewNoSSR theme="dark" height={"512px"} width={"100%"} />;
+  const poolValue = usePoolStore((store) => store.pool);
+  const pool = pools.find((pool) => pool.value === poolValue);
+
+  const symbol = pool ? pool.underlyingLabel + pool.accountingLabel : "BTCUSDT";
+
+  console.log("symbol", symbol);
+
+  return (
+    <SymbolOverviewNoSSR
+      theme="dark"
+      height={"512px"}
+      width={"100%"}
+      symbol={symbol}
+    />
+  );
 };
