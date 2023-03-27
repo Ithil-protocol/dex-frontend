049f2c75a1b5896c49c94a4626b47f01abc052a6
dev-aly3n <dev.aly3n@gmail.com>
2023-03-27 13:19:06 +0330

    panel(feat): add copyright caption

--- src/components/CandlestickChart/CandlestickChart.tsx
+++ src/components/CandlestickChart/CandlestickChart.tsx
@@ -1,4 +1,6 @@
+import { Box, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
+import styles from "./CandlestickChart.module.scss";
 const AdvancedRealTimeChartNoSSR = dynamic(
   () =>
     import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
@@ -8,5 +10,23 @@ const AdvancedRealTimeChartNoSSR = dynamic(
 );
 
 export const CandlestickChart = () => {
-  return <AdvancedRealTimeChartNoSSR theme="dark" height={"512px"} width={"100%"} />;
+  return (
+    <Box height={"100%"}>
+      <AdvancedRealTimeChartNoSSR
+        theme="dark"
+        autosize
+        symbol="BTCUSDT"
+        copyrightStyles={{ parent: { display: "none" } }}
+      />
+      <Typography variant="caption" className={styles.copyright} >
+        <a
+          href={`https://www.tradingview.com/symbols/${"BTCUSDT"}`}
+          target={"_blank"}
+        >
+          {"BTCUSDT"} Chart
+        </a>
+        <p>by TradingView</p>
+      </Typography>
+    </Box>
+  );
 };
