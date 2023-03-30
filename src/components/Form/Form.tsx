bfb2940cd66a6634c28596e3a2b43ef272309851
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-30 07:13:52 +0330

    Form(feat): implement

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+import AmountField from "./AmountField";
+import Boost from "./Boost";
+import HoldingField from "./HoldingField";
+import SubmitButton from "./SubmitButton";
+import TotalField from "./TotalField";
+
 const Form = () => {
   return (
-    <div>
-      <h2>Form</h2>
+    <div
+      style={{
+        backgroundColor: "#284f5b",
+        display: "flex",
+        flexDirection: "column",
+        gap: 5,
+        padding: 10,
+      }}
+    >
+      <HoldingField />
+      <AmountField />
+      <div style={{ marginTop: 10 }}></div>
+      <Boost />
+      <TotalField />
+      <SubmitButton />
     </div>
   );
 };
