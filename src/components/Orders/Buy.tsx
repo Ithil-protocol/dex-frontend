432e68b5342b09fe9f9f5b46828b877558b89180
dev-aly3n <dev.aly3n@gmail.com>
2023-04-01 02:06:12 +0330

    config(wagmi): fix

--- src/components/Orders/Buy.tsx
+++ src/components/Orders/Buy.tsx
@@ -1,11 +1,10 @@
 import type { BigNumberish } from "ethers";
 import { contractABI } from "store/abi";
-import { ContractInputs } from "types";
 import { useContractReads } from "wagmi";
 import Order from "./Order";
 
 export const Buy = () => {
-  const priceLevelsList: ContractInputs[] = [...Array(8)].map((el, index) => {
+  const priceLevelsList = [...Array(8)].map((el, index) => {
     return {
       abi: contractABI,
       address: "0x3ff417dACBA7F0bb7673F8c6B3eE68D483548e37",
