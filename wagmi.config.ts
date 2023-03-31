fa2d4c2d98fc768adec403e109df977128211275
dev-aly3n <dev.aly3n@gmail.com>
2023-04-01 02:26:07 +0330

    config(wagmi): generate hooks

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
-import { contractABI } from "store/abi";
+import { erc20ABI } from "wagmi";
+import { contractABI } from "./src/store/abi";
 
-export default defineConfig({
-  out: "src/hooks/contracts/pools.ts",
-  contracts: [
-    {
-      abi: contractABI,
-      name: "pools",
-    },
-  ],
-  plugins: [react()],
-});
+export default defineConfig([
+  {
+    out: "src/hooks/contracts/pool.ts",
+    contracts: [
+      {
+        abi: contractABI,
+        name: "pool",
+      },
+    ],
+    plugins: [react()],
+  },
+  {
+    out: "src/hooks/contracts/token.ts",
+    contracts: [
+      {
+        abi: erc20ABI,
+        name: "token",
+      },
+    ],
+    plugins: [react()],
+  },
+]);
