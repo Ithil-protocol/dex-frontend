97d2c35c3e42c8aefc78a04e96f4718db4123fe7
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-02 04:50:12 +0330

    Boost(feat): add sample state, new label

--- src/components/CreateOrder/Form/Fields/Boost.tsx
+++ src/components/CreateOrder/Form/Fields/Boost.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "./Slider";
 
 const Boost = () => {
+  const [boost, setBoost] = useState(0);
+
+  const handleChangeBoost = (
+    _event: Event,
+    value: number | number[],
+    _activeThumb: number
+  ) => {
+    setBoost(value as number);
+  };
+
   return (
     <div style={{ padding: "0px 10px 0px" }}>
-      Boost
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <span style={{ color: "#e3e3e378" }}>Boost</span>
+        <span>{boost}</span>
+      </div>
       <Slider
         valueLabelDisplay="auto"
-        defaultValue={0}
+        value={boost}
         step={0.01}
         min={0}
         max={0.1}
+        onChange={handleChangeBoost}
+        valueLabelFormat={(value) => `${value} ETH`}
       />
     </div>
   );
