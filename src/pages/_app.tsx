d3d910f246095b40f62e90349fe700c3fefffd87
dev-aly3n <dev.aly3n@gmail.com>
2023-04-02 19:26:08 +0330

    pages(feat): add ReactQueryProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,16 @@
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ReactQueryProvider from "Providers/ReactQueryProvider";
 import Web3Provider from "Providers/Web3Provider";
 import { AppProps } from "next/app";
-import { useState } from "react";
 import createEmotionCache from "styles/createEmotionCache";
 import "styles/global.scss";
 import theme from "styles/theme";
-import { WagmiConfig, configureChains, createClient, goerli } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { infuraProvider } from "wagmi/providers/infura";
-import { publicProvider } from "wagmi/providers/public";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
-const reactQueryConfig = {
-  defaultOptions: {
-    queries: {
-      staleTime: 1 * 60 * 60 * 1000,
-      cacheTime: 6 * 60 * 60 * 1000,
-      retry: false,
-      refetchOnWindowFocus: true,
-    },
-  },
-};
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -35,10 +18,9 @@ export interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const [queryClient] = useState(() => new QueryClient(reactQueryConfig));
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <ReactQueryProvider>
       <Web3Provider>
         <ReactQueryDevtools />
         <CacheProvider value={emotionCache}>
@@ -50,6 +32,6 @@ export default function MyApp(props: MyAppProps) {
           </ThemeProvider>
         </CacheProvider>
       </Web3Provider>
-    </QueryClientProvider>
+    </ReactQueryProvider>
   );
 }
