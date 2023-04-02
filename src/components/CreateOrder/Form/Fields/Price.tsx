34a28afcc534d740fa3ef2dde6d1eb2ea0dcec7b
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-02 04:48:55 +0330

    Price(fix): style, end icon

--- src/components/CreateOrder/Form/Fields/Price.tsx
+++ src/components/CreateOrder/Form/Fields/Price.tsx
@@ -1,12 +1,23 @@
-import { TextField } from "@mui/material";
+import { InputAdornment, TextField } from "@mui/material";
 import React from "react";
 
-const Price = () => {
+interface Props {
+  endLabel: string;
+}
+
+const Price: React.FC<Props> = ({ endLabel }) => {
   return (
     <TextField
       variant="filled"
       placeholder="Price"
-      InputProps={{ disableUnderline: true }}
+      InputProps={{
+        disableUnderline: true,
+        endAdornment: (
+          <InputAdornment position="start">
+            <span style={{ color: "white" }}>{endLabel}</span>
+          </InputAdornment>
+        ),
+      }}
       size="small"
       fullWidth
     />
