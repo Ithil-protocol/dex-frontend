6a37254c80c2355849919fb558e2113e4416ea8e
dev-aly3n <dev.aly3n@gmail.com>
2023-04-04 23:56:54 +0330

    panel(fix): fix form amount input

--- src/components/CreateOrder/Form/Fields/Price.tsx
+++ src/components/CreateOrder/Form/Fields/Price.tsx
@@ -1,8 +1,9 @@
 import { InputAdornment, TextField } from "@mui/material";
+import FormGroup from "@mui/material/FormGroup/FormGroup";
+import InputLabel from "@mui/material/InputLabel";
 import { decimalRegex } from "data/regex";
 import React, { ChangeEvent, useState } from "react";
 import { useController } from "react-hook-form";
-
 interface Props {
   endLabel: string;
   control: any;
@@ -29,32 +30,35 @@ export default React.forwardRef<HTMLDivElement, Props>(function Price(
   const [value, setValue] = useState(inputProps.value);
 
   return (
-    <TextField
-      {...inputProps}
-      onChange={(event) => {
-        if (decimalRegex.test(event.target.value)) {
-          onChange(Number(event.target.value));
-          setValue(event.target.value);
-        }
-      }}
-      value={value}
-      inputRef={ref1}
-      size="small"
-      variant="filled"
-      placeholder="Price"
-      autoComplete="off"
-      error={!!error}
-      helperText={error ? "Please enter a valid number" : ""}
-      InputProps={{
-        disableUnderline: true,
-        endAdornment: (
-          <InputAdornment position="end">
-            <span style={{ color: "white" }}>{props.endLabel}</span>
-          </InputAdornment>
-        ),
-      }}
-      fullWidth
-      required
-    />
+    <FormGroup>
+      <InputLabel htmlFor="price"> Price</InputLabel>
+      <TextField
+        {...inputProps}
+        onChange={(event) => {
+          if (decimalRegex.test(event.target.value)) {
+            onChange(Number(event.target.value));
+            setValue(event.target.value);
+          }
+        }}
+        value={value}
+        inputRef={ref1}
+        size="small"
+        variant="outlined"
+        id="price"
+        autoComplete="off"
+        error={!!error}
+        helperText={error ? "Please enter a valid number" : ""}
+        InputProps={{
+          disableUnderline: true,
+          endAdornment: (
+            <InputAdornment position="end">
+              <span style={{ color: "white" }}>{props.endLabel}</span>
+            </InputAdornment>
+          ),
+        }}
+        fullWidth
+        required
+      />
+    </FormGroup>
   );
 });
