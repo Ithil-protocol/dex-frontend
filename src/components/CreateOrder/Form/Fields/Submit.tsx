458269f05a8658c514df0b951fe82417d59aa6ba
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-04 03:55:57 +0330

    Submit(feat): refactor with RHF

--- src/components/CreateOrder/Form/Fields/Submit.tsx
+++ src/components/CreateOrder/Form/Fields/Submit.tsx
@@ -1,19 +1,29 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { Control, useWatch } from "react-hook-form";
 
 interface Props {
   label: string;
+  control: Control<any, any>;
 }
 
-const Submit: React.FC<Props> = ({ label }) => {
+const Submit: React.FC<Props> = ({ label, control }) => {
+  const formValues = useWatch({ control });
+
   return (
     <Button
       fullWidth
-      style={{
+      disabled={!formValues.price || !formValues.amount}
+      sx={{
         backgroundColor: "#8a8a8a",
         color: "white",
         textTransform: "none",
+        ":disabled": {
+          color: "#ffffff7d",
+          backgroundColor: "#787878",
+        },
       }}
+      type="submit"
     >
       Buy {label}
     </Button>
