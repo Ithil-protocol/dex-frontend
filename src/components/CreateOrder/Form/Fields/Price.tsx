63dfd364ef6a11890a145d6e8260bdb193bd54ac
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-04 03:56:23 +0330

    Price(feat): Refactor with RHF

--- src/components/CreateOrder/Form/Fields/Price.tsx
+++ src/components/CreateOrder/Form/Fields/Price.tsx
@@ -3,25 +3,49 @@ import React from "react";
 
 interface Props {
   endLabel: string;
+  onKeyDown: React.KeyboardEventHandler<
+    HTMLDivElement | HTMLTextAreaElement | HTMLInputElement
+  >;
 }
 
-const Price: React.FC<Props> = ({ endLabel }) => {
+export default React.forwardRef<HTMLDivElement, Props>(function Price(
+  props,
+  ref
+) {
   return (
     <TextField
+      {...props}
+      ref={ref}
+      name="price"
+      type="number"
       variant="filled"
       placeholder="Price"
+      autoComplete="off"
       InputProps={{
+        onKeyDown: props.onKeyDown,
         disableUnderline: true,
         endAdornment: (
           <InputAdornment position="start">
-            <span style={{ color: "white" }}>{endLabel}</span>
+            <span style={{ color: "white" }}>{props.endLabel}</span>
           </InputAdornment>
         ),
+        sx: {
+          "& input[type=number]": {
+            "-moz-appearance": "textfield",
+          },
+          "& input[type=number]::-webkit-outer-spin-button": {
+            "-webkit-appearance": "none",
+            margin: 0,
+          },
+          "& input[type=number]::-webkit-inner-spin-button": {
+            "-webkit-appearance": "none",
+            margin: 0,
+          },
+        },
       }}
       size="small"
       fullWidth
+      required
     />
   );
-};
-
-export default Price;
+});
