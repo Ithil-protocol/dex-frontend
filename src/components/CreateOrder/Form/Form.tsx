688cbde3a980c4d7c4ff44d473bbd30be5347c8d
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-05 07:06:54 +0330

    fix form gap

--- src/components/CreateOrder/Form/Form.tsx
+++ src/components/CreateOrder/Form/Form.tsx
@@ -11,7 +11,6 @@ import Price from "./Fields/Price";
 import Submit from "./Fields/Submit";
 import Total from "./Fields/Total";
 import { useAccount, useBalance } from "wagmi";
-import { Box } from "@mui/material";
 
 const Form = () => {
   const { control, handleSubmit, setValue } = useForm();
@@ -29,38 +28,37 @@ const Form = () => {
   const handleFormSubmit = (data: StringMap) => console.log(data);
 
   return (
-    <Box
-      sx={(_theme) => ({
+    <form
+      style={{
         display: "flex",
         flexDirection: "column",
         gap: 5,
         padding: "10px",
-      })}
+      }}
+      onSubmit={handleSubmit(handleFormSubmit)}
     >
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <Price control={control} endLabel={pool?.accountingLabel || ""} />
+      <Price control={control} endLabel={pool?.accountingLabel || ""} />
 
-        <AmountTextField
-          endLabel={pool?.underlyingLabel || ""}
-          control={control}
-        />
+      <AmountTextField
+        endLabel={pool?.underlyingLabel || ""}
+        control={control}
+      />
 
-        <AmountSlider setValue={setValue} />
+      <AmountSlider setValue={setValue} />
 
-        <Available
-          endLabel={pool?.underlyingLabel || ""}
-          available={formatBigNumber(available.current)}
-        />
+      <Available
+        endLabel={pool?.underlyingLabel || ""}
+        available={formatBigNumber(available.current)}
+      />
 
-        <div style={{ marginTop: 5 }}></div>
+      <div style={{ marginTop: 5 }}></div>
 
-        <Boost control={control} />
+      <Boost control={control} />
 
-        <Total control={control} label={pool?.accountingLabel || ""} />
+      <Total control={control} label={pool?.accountingLabel || ""} />
 
-        <Submit control={control} label={pool?.underlyingLabel || ""} />
-      </form>
-    </Box>
+      <Submit control={control} label={pool?.underlyingLabel || ""} />
+    </form>
   );
 };
 
