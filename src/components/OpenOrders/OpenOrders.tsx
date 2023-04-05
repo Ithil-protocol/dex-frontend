175a88561b459d614e514a5b8ab0b48906b375da
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-05 15:56:30 +0330

    implement open order tabs

--- src/components/OpenOrders/OpenOrders.tsx
+++ src/components/OpenOrders/OpenOrders.tsx
@@ -1,7 +1,56 @@
+import { Box } from "@mui/material";
+import Orders from "./Orders/Orders";
+import CustomTabs from "components/common/Tabs";
+import TabPanel from "components/common/Panel";
+import CustomTab from "components/common/Tab";
+import React from "react";
+import { openOrdersData } from "data/openOrders";
+
 export const OpenOrders = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h2>Open Orders</h2>
-    </div>
+    <Box
+      sx={(theme) => ({
+        bgcolor: theme.palette.background.paper,
+        borderRadius: "5px",
+        gap: "5px",
+        height: "100%",
+        overflowY: "clip",
+        maxHeight: "300px",
+      })}
+    >
+      <Box
+        sx={(theme) => ({
+          bgcolor: theme.palette.background.paper,
+          borderRadius: "5px",
+          padding: "10px",
+        })}
+      >
+        <Box>
+          <CustomTabs value={value} onChange={handleChange}>
+            <CustomTab label="Open Orders" />
+            <CustomTab label="Orders History" />
+          </CustomTabs>
+        </Box>
+
+        <Box
+          sx={{
+            padding: "10px 5px 5px 5px",
+          }}
+        >
+          <TabPanel value={value} index={0}>
+            <Orders openOrdersData={openOrdersData} hasCancel />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Orders openOrdersData={openOrdersData} hasCancel={false} />
+          </TabPanel>
+        </Box>
+      </Box>
+    </Box>
   );
 };
