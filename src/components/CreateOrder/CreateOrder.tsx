d62aab5f2d276e23d13890a5d682693d59442d7c
dev-aly3n <dev.aly3n@gmail.com>
2023-04-07 00:27:25 +0330

    fix name

--- src/components/CreateOrder/CreateOrder.tsx
+++ src/components/CreateOrder/CreateOrder.tsx
@@ -1,7 +1,39 @@
-import EachMarketTab from "./MarketTabs/EachMarketTab";
+import { useState } from "react";
+import WrapperTabs from "components/common/Tabs";
+import WrapperTab from "components/common/Tab";
+import { Box } from "@mui/material";
+import TabPanel from "components/common/TabPanel";
+import PoolTabs from "./PoolTabs/PoolTabs";
 
 const CreateOrder = () => {
-  return <EachMarketTab />;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={(theme) => ({
+        bgcolor: theme.palette.background.paper,
+        borderRadius: "5px",
+        padding: "10px 5px 5px 5px",
+      })}
+    >
+      <Box>
+        <WrapperTabs value={value} onChange={handleChange}>
+          <WrapperTab label="Limit" />
+          <WrapperTab label="Market" />
+        </WrapperTabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <PoolTabs />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <PoolTabs />
+      </TabPanel>
+    </Box>
+  );
 };
 
 export default CreateOrder;
