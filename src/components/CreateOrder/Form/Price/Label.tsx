a676950ae7e4334dae347c3f6d8afb8bd84804d3
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-11 22:00:40 +0330

    PriceLabel(feat): implement

---
+++ src/components/CreateOrder/Form/Price/Label.tsx
@@ -0,0 +1,8 @@
+import WrapperInputLabel from "components/common/InputLabel";
+import React from "react";
+
+const PriceLabel = () => {
+  return <WrapperInputLabel label="Price" tooltip="Price" htmlFor="price" />;
+};
+
+export default PriceLabel;
