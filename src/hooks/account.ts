c2938851319aeb32fc189b7c2d17d8a71343bcd8
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-15 01:50:15 +0330

    implement useGetBlock hook

--- src/hooks/account.ts
+++ src/hooks/account.ts
@@ -1,4 +1,7 @@
+import { BigNumber, BigNumberish, Event, utils } from "ethers";
+import { useEffect, useState } from "react";
 import { useAccount, useBalance } from "wagmi";
+import { usePoolOrders } from "./contracts/pool";
 
 interface TokenBalanceProps {
   tokenAddress: `0x${string}` | undefined;
@@ -10,4 +13,22 @@ export const useTokenBalance = ({ tokenAddress }: TokenBalanceProps) => {
     address,
     token: tokenAddress,
   });
+};
+
+export const useGetBlock = (event: Event) => {
+  const [block, setBlock] = useState<{ timestamp: any }>({
+    timestamp: 0,
+  });
+
+  useEffect(() => {
+    const fn = async () => {
+      const block = await event.getBlock();
+      setBlock(block);
+    };
+
+    fn();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [event]);
+
+  return block;
 };
