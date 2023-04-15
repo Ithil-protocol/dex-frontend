8d85a47b425c401ca1a256c40784d22c5da110f4
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2023-04-16 00:22:15 +0330

    feat : improve pre-build script

--- src/scripts/pre-build.mjs
+++ src/scripts/pre-build.mjs
@@ -9,7 +9,9 @@ import { rawContractABI } from "../store/abi-raw.mjs";
 
 import addresses from "../data/addresses.json" assert { type: "json" };
 
-const { chains, provider, webSocketProvider } = configureChains(
+console.log("creating pairs...");
+
+const { provider } = configureChains(
   [goerli],
   [
     alchemyProvider({ apiKey: "ZLR2ae9uziqXiA-4OM8RB13sqjuMRVHy" }),
@@ -18,33 +20,37 @@ const { chains, provider, webSocketProvider } = configureChains(
   ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "ithil dex",
-  chains,
-});
-
 createClient({
   autoConnect: true,
   provider,
-  webSocketProvider,
-  connectors,
 });
 
 (async () => {
   let newPools = [...addresses];
   const addressesLength = addresses.length;
 
-  const underlyingContracts = addresses.map((item) => ({
+  const underlyingContracts = addresses.flatMap((item) => [{
     abi: rawContractABI,
     functionName: "underlying",
-    address: item.address,
-  }));
+    address: item.sell.address,
+  },
+  {
+    abi: rawContractABI,
+    functionName: "underlying",
+    address: item.buy.address,
+  }]);
 
-  const accountingContracts = addresses.map((item) => ({
+  const accountingContracts = addresses.flatMap((item) => [{
     abi: rawContractABI,
     functionName: "accounting",
-    address: item.address,
-  }));
+    address: item.sell.address,
+  },
+  {
+    abi: rawContractABI,
+    functionName: "accounting",
+    address: item.buy.address,
+  }]);
+
 
   const underlyingPromise = await readContracts({
     contracts: underlyingContracts,
@@ -54,34 +60,42 @@ createClient({
     contracts: accountingContracts,
   });
 
-  console.log("underlyingPromise", underlyingPromise);
-
-  const tokens = [...underlyingPromise, ...accountingPromise];
-
-  const tokenContracts = tokens.map((item) => ({
+  const underlyingTokensContracts = underlyingPromise.map((item) => ({
     abi: erc20ABI,
     functionName: "decimals",
     address: item,
   }));
 
-  const tokensDecimals = await readContracts({
-    contracts: tokenContracts,
+  const accountingTokensContracts = accountingPromise.map((item) => ({
+    abi: erc20ABI,
+    functionName: "decimals",
+    address: item,
+  }));
+
+  const underlyingTokensDecimals = await readContracts({
+    contracts: underlyingTokensContracts,
+  });
+
+  const accountingTokensDecimals = await readContracts({
+    contracts: accountingTokensContracts,
   });
 
   for (let i = 0; i < addressesLength; i++) {
-    newPools[i].sell.underlying.address = underlyingPromise[i];
-    newPools[i].sell.accounting.address = accountingPromise[i];
-    newPools[i].buy.accounting.address = underlyingPromise[i];
-    newPools[i].buy.underlying.address = accountingPromise[i];
-    newPools[i].sell.underlying.decimals = tokensDecimals[i * 2];
-    newPools[i].buy.underlying.decimals = tokensDecimals[i * 2 + 1];
-    newPools[i].sell.accounting.decimals = tokensDecimals[i * 2 + 1];
-    newPools[i].buy.accounting.decimals = tokensDecimals[i * 2];
+
+    const sellIndex = i * 2;
+    const buyIndex = i * 2 + 1;
+
+    newPools[i].sell.underlying.address = underlyingPromise[sellIndex];
+    newPools[i].buy.underlying.address = underlyingPromise[buyIndex];
+    newPools[i].sell.accounting.address = accountingPromise[sellIndex];
+    newPools[i].buy.accounting.address = accountingPromise[buyIndex];
+    newPools[i].sell.underlying.decimals = underlyingTokensDecimals[sellIndex];
+    newPools[i].buy.underlying.decimals = underlyingTokensDecimals[buyIndex];
+    newPools[i].sell.accounting.decimals = accountingTokensDecimals[sellIndex];
+    newPools[i].buy.accounting.decimals = accountingTokensDecimals[buyIndex];
   }
 
   fs.writeFileSync("./src/data/pools.json", JSON.stringify(newPools));
 
-  console.log("tokensDecimals", tokensDecimals);
+  console.log("pairs created");
 })();
-
-console.log("test");
