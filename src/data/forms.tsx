fb9eac3b73fbd2b68ad24bb047d638ae7e996ab9
dev-aly3n <dev.aly3n@gmail.com>
2023-04-16 15:40:11 +0330

    form(feat): add market schema

--- src/data/forms.tsx
+++ src/data/forms.tsx
@@ -1,8 +1,12 @@
-import { LimitInputs } from "types";
+import { LimitInputs, MarketInputs } from "types";
 import { object, string, ObjectSchema } from "yup";
 
 export const limitSchema: ObjectSchema<LimitInputs> = object().shape({
   Price: string().required(),
   Amount: string().required(),
   Boost: string().required(),
+});
+
+export const marketSchema: ObjectSchema<MarketInputs> = object().shape({
+  Amount: string().required(),
 });
