95ecb331582e888380e9edae6edf943574eddb16
dev-aly3n <dev.aly3n@gmail.com>
2023-04-16 15:41:17 +0330

    fix

--- src/data/forms.tsx
+++ src/data/forms.tsx
@@ -1,12 +1,16 @@
 import { LimitInputs, MarketInputs } from "types";
 import { object, string, ObjectSchema } from "yup";
 
-export const limitSchema: ObjectSchema<LimitInputs> = object().shape({
-  Price: string().required(),
-  Amount: string().required(),
-  Boost: string().required(),
-});
+export const limitSchema: ObjectSchema<LimitInputs> = object()
+  .shape({
+    Price: string().required(),
+    Amount: string().required(),
+    Boost: string().required(),
+  })
+  .required();
 
-export const marketSchema: ObjectSchema<MarketInputs> = object().shape({
-  Amount: string().required(),
-});
+export const marketSchema: ObjectSchema<MarketInputs> = object()
+  .shape({
+    Amount: string().required(),
+  })
+  .required();
