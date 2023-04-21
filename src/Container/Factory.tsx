f794cbedd0fd5d37b171ff6d2f07ef13381eb911
dev-aly3n <dev.aly3n@gmail.com>
2023-04-21 12:24:03 +0330

    factory(feat): add generate pool

--- src/Container/Factory.tsx
+++ src/Container/Factory.tsx
@@ -1,10 +1,17 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { LoadingButton } from "@mui/lab";
 import { Box, CircularProgress, Input, TextField } from "@mui/material";
+import TransactionToast from "components/Common/Toast/TransactionToast";
 import { factorySchema } from "data/forms";
-import { useFactoryPools } from "hooks/contracts/factory";
+import {
+  useFactoryCreatePool,
+  useFactoryPools,
+  usePrepareFactoryCreatePool,
+} from "hooks/contracts/factory";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import { FactoryInputs } from "types";
+import { useWaitForTransaction } from "wagmi";
 
 interface Props {}
 
@@ -18,21 +25,40 @@ const Factory: React.FC<Props> = () => {
     resolver: yupResolver(factorySchema),
   });
   const { data } = useFactoryPools({
-    address: "0xCC1ec8857882feF6637bcB6a4344c51aF2807822",
+    address: "0x2971eDA359e35E2750e389aD2A87e3AB4CD8eb09",
     args: [
       getValues("underlyingAddress") as `0x${string}`,
       getValues("accountingAddress") as `0x${string}`,
     ],
+    cacheTime: 0,
+    watch: true,
   });
-  console.log(getValues("accountingAddress"));
+  const { config } = usePrepareFactoryCreatePool({
+    address: "0x2971eDA359e35E2750e389aD2A87e3AB4CD8eb09",
+    args: [
+      getValues("underlyingAddress") as `0x${string}`,
+      getValues("accountingAddress") as `0x${string}`,
+      1,
+    ],
+  });
+  const { data: writeData, write } = useFactoryCreatePool({
+    ...config,
+  });
+  const { data: waitedData } = useWaitForTransaction({
+    hash: writeData?.hash,
+    onSuccess: (data) => {
+      toast.success(
+        <TransactionToast
+          text="Pool generated successfully"
+          hash={data.transactionHash}
+        />
+      );
+    },
+  });
+  console.log(waitedData);
 
-  const handleFormSubmit = (data) => {
-    console.log(data);
-    // if (approve) {
-    //   approve();
-    //   return;
-    // }
-    // write?.();
+  const handleFormSubmit = (e) => {
+    console.log(e);
   };
 
   return (
@@ -79,7 +105,34 @@ const Factory: React.FC<Props> = () => {
           >
             Get
           </LoadingButton>
-          <Input sx={{ width: 520 }} color="secondary" readOnly value={data} />
+          {/* <LoadingButton
+            variant="contained"
+            endIcon={
+              isSubmitting && <CircularProgress size={22} color="inherit" />
+            }
+            loading={isSubmitting}
+            fullWidth
+            sx={{
+              textTransform: "none",
+              width: "100%",
+            }}
+            type="submit"
+            onClick={()=>{write?.()}}
+          >
+            Generate
+          </LoadingButton> */}
+          <TextField
+            sx={{ width: 520 }}
+            color="secondary"
+            value={data ?? "Get an exist pool address"}
+            disabled={true}
+          />
+          <TextField
+            sx={{ width: 520 }}
+            color="secondary"
+            disabled={true}
+            value={waitedData ?? "Generate new pool address"}
+          />
         </div>
       </form>
     </Box>
