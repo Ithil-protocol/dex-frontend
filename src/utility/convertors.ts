d3d2e4758b4288b396888b90ab2bdb3e44d38a1c
dev-aly3n <dev.aly3n@gmail.com>
2023-04-24 10:50:16 +0330

    market trades(feat): add buy amount converter

--- src/utility/convertors.ts
+++ src/utility/convertors.ts
@@ -5,3 +5,8 @@ export const buyPriceConverter = (price: BigNumber, pool: Pool) => {
   // pool is equal to buyPool
   return Number(utils.formatUnits(price, pool.underlying.decimals));
 };
+
+export const buyAmountConverter = (amount: BigNumber, pool: Pool) => {
+  // pool is equal to buyPool
+  return Number(utils.formatUnits(amount, pool.underlying.decimals));
+};
