2ed0b33e92c64d51e320620005933ba2f7c077db
dev-aly3n <dev.aly3n@gmail.com>
2023-04-24 10:52:53 +0330

    market trades(feat): add sell price converter

--- src/utility/convertors.ts
+++ src/utility/convertors.ts
@@ -10,3 +10,13 @@ export const buyAmountConverter = (amount: BigNumber, pool: Pool) => {
   // pool is equal to buyPool
   return Number(utils.formatUnits(amount, pool.underlying.decimals));
 };
+
+export const sellPriceConverter = (price: BigNumber, pool: Pool) => {
+  // pool is equal to sellPool
+  return 1 / Number(utils.formatUnits(price, pool.underlying.decimals));
+};
+
+export const sellAmountConverter = (amount: BigNumber, pool: Pool) => {
+  // pool is equal to sellPool
+  return Number(utils.formatUnits(amount, pool.underlying.decimals));
+};
