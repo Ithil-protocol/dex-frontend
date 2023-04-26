566e6faf720d5e4b95b30f30b6c3ae9728e66314
dev-aly3n <dev.aly3n@gmail.com>
2023-04-26 22:53:22 +0330

    form(fix): add label next to price

--- src/components/CreateOrder/Inputs/Price/Label.tsx
+++ src/components/CreateOrder/Inputs/Price/Label.tsx
@@ -1,7 +1,24 @@
+import { Chip } from "@mui/material";
 import WrapperInputLabel from "components/Common/WrapperInputLabel";
+import { usePoolStore } from "store";
 
 const PriceLabel = () => {
-  return <WrapperInputLabel label="Price" tooltip="Price" htmlFor="price" />;
+  const [defaultPool, pair] = usePoolStore((state) => [
+    state.default,
+    state.pair,
+  ]);
+  const ChipComponent = (
+    <Chip icon={defaultPool.accounting.icon} label={pair.accountingLabel} />
+  );
+
+  return (
+    <WrapperInputLabel
+      label="Price"
+      tooltip="Price"
+      htmlFor="price"
+      Currency={ChipComponent}
+    />
+  );
 };
 
 export default PriceLabel;
