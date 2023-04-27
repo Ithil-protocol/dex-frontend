9482599fe85091f4e7255cb5f9efa301a038724a
dev-aly3n <dev.aly3n@gmail.com>
2023-04-27 18:41:43 +0330

    remove panel page and transfer panel component to home page

--- src/pages/panel.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Panel = dynamic(() => import("Container/Panel").then((w) => w.default), {
-  ssr: false,
-});
-
-export default function panel() {
-  return <Panel />;
-}
