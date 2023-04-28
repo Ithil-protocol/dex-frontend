31ffd6124a5e14a182258502d262d75e4e066e39
dev-aly3n <dev.aly3n@gmail.com>
2023-04-28 19:41:02 +0330

    pair(feat): add icons to root of project

---
+++ icons.ts
@@ -0,0 +1,11 @@
+import ETH from "cryptocurrency-icons/svg/icon/eth.svg";
+import USDC from "cryptocurrency-icons/svg/icon/usdc.svg";
+import Generic from "cryptocurrency-icons/svg/icon/generic.svg";
+
+//the "keies" in icons object must match underlyingLabels or accountingLabels
+
+export const icons = {
+  WETH: ETH,
+  FAUCET: Generic,
+  USDC,
+};
