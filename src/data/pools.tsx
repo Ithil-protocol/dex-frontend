3b02fdbbad44aab87f224a870928e0b7d960da54
dev-aly3n <dev.aly3n@gmail.com>
2023-04-28 17:13:58 +0330

    pair(feat): change icons to a better format

--- src/data/pools.tsx
+++ src/data/pools.tsx
@@ -1,49 +1,14 @@
-import DAI from "cryptocurrency-icons/svg/icon/dai.svg";
 import ETH from "cryptocurrency-icons/svg/icon/eth.svg";
 import USDC from "cryptocurrency-icons/svg/icon/usdc.svg";
-import WBTC from "cryptocurrency-icons/svg/icon/wbtc.svg";
-import Generic from "cryptocurrency-icons/svg/icon/generic.svg";
+import FAUCET from "cryptocurrency-icons/svg/icon/generic.svg";
 import { Pair } from "types";
 import rawPools from "./pools.json";
 
-const poolsIcons = [
-  {
-    sell: {
-      underlying: {
-        icon: <ETH />,
-      },
-      accounting: {
-        icon: <USDC />,
-      },
-    },
-    buy: {
-      underlying: {
-        icon: <USDC />,
-      },
-      accounting: {
-        icon: <ETH />,
-      },
-    },
-  },
-  {
-    sell: {
-      underlying: {
-        icon: <ETH />,
-      },
-      accounting: {
-        icon: <Generic />,
-      },
-    },
-    buy: {
-      underlying: {
-        icon: <Generic />,
-      },
-      accounting: {
-        icon: <ETH />,
-      },
-    },
-  },
-];
+const icons = {
+  WETH: <ETH />,
+  FAUCET: <FAUCET />,
+  USDC: <USDC />,
+};
 
 export const pairs: Pair[] = rawPools.map((item, index) => ({
   ...item,
@@ -52,12 +17,12 @@ export const pairs: Pair[] = rawPools.map((item, index) => ({
     ...item.sell,
     underlying: {
       ...item.sell.underlying,
-      ...poolsIcons[index].sell.underlying,
+      icon: icons[item.underlyingLabel],
       address: item.sell.underlying.address as `0x${string}`,
     },
     accounting: {
       ...item.sell.accounting,
-      ...poolsIcons[index].sell.accounting,
+      icon: icons[item.accountingLabel],
       address: item.sell.accounting.address as `0x${string}`,
     },
     address: item.sell.address as `0x${string}`,
@@ -66,12 +31,12 @@ export const pairs: Pair[] = rawPools.map((item, index) => ({
     ...item.buy,
     underlying: {
       ...item.buy.underlying,
-      ...poolsIcons[index].buy.underlying,
+      icon: icons[item.accountingLabel],
       address: item.buy.underlying.address as `0x${string}`,
     },
     accounting: {
       ...item.buy.accounting,
-      ...poolsIcons[index].buy.accounting,
+      icon: icons[item.underlyingLabel],
       address: item.buy.accounting.address as `0x${string}`,
     },
     address: item.buy.address as `0x${string}`,
