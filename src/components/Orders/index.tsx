85ed666a3bef3fe0fd72e85690d3f485640c99be
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2023-05-01 02:56:21 +0330

    add header to orderbook

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,10 +1,17 @@
 import Buy from "./Buy";
 import Sell from "./Sell";
 import styles from "./Orders.module.scss";
+import { usePoolStore } from "store";
 
 const Orders = () => {
+  const pair = usePoolStore((store) => store.pair);
+
   return (
     <div className={styles.orders}>
+      <div className={styles.header}>
+        <span>price ({pair.underlyingLabel})</span>
+        <span>amount ({pair.accountingLabel})</span>
+      </div>
       <Buy />
       <p className={styles.last}>9854.236</p>
       <Sell />
