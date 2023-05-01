b8165c5a7b214e27d35da13bc5806424ba07a1c6
dev-aly3n <dev.aly3n@gmail.com>
2023-05-02 03:00:13 +0330

    form(feat): add close handler to confirmation

--- src/components/CreateOrder/Confirmation/LimitConfirmation.tsx
+++ src/components/CreateOrder/Confirmation/LimitConfirmation.tsx
@@ -5,6 +5,7 @@ import { Dispatch, SetStateAction } from "react";
 import { usePoolStore } from "store";
 import { LimitFinalValues } from "types";
 import styles from "./LimitConfirmation.module.scss";
+import { useGetConvertersBySide } from "hooks/converters";
 interface Props {
   finalValues: LimitFinalValues;
   write: (() => void) | undefined;
@@ -12,21 +13,35 @@ interface Props {
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const LimitConfirmation: React.FC<Props> = ({ finalValues, open, write }) => {
+const LimitConfirmation: React.FC<Props> = ({
+  finalValues,
+  open,
+  setOpen,
+  write,
+}) => {
   const [side, pool] = usePoolStore((state) => [state.side, state.pool]);
 
+  const converters = useGetConvertersBySide(side);
   const { data: preview, isLoading } = usePoolPreviewOrder({
     address: pool.address,
     args: [finalValues.price, finalValues.boost],
   });
 
+  const closeHandler = () => {
+    setOpen(false);
+  };
+
   return (
-    <Dialog open={open} fullWidth maxWidth={"sm"}>
+    <Dialog open={open} onClose={closeHandler} fullWidth maxWidth={"sm"}>
       <DialogTitle align="center">Limit order confirmation</DialogTitle>
       <Box display={"flex"} flexDirection={"column"} px={6} py={3}>
         <div className={styles.price}>
           <span>Actual price</span>
-          {isLoading ? <Skeleton height={20} /> : <span></span>}
+          {isLoading ? (
+            <Skeleton height={20} />
+          ) : (
+            <span>{converters.priceConverter(preview!.actualPrice)}</span>
+          )}
         </div>
       </Box>
     </Dialog>
