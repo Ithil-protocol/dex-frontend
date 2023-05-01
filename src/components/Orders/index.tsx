e833aa72f30c3b6c1abfa2a9c2188d0af1964fe4
dev-aly3n <dev.aly3n@gmail.com>
2023-05-01 20:12:05 +0330

    panel(feat): add latest trade to the orders

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -2,10 +2,13 @@ import Buy from "./Buy";
 import Sell from "./Sell";
 import styles from "./Orders.module.scss";
 import { usePoolStore } from "store";
+import { useLatestTrade } from "hooks/events";
+import { Typography } from "@mui/material";
 
 const Orders = () => {
   const pair = usePoolStore((store) => store.pair);
 
+  const latestPrice = useLatestTrade();
   return (
     <div className={styles.orders}>
       <div className={styles.header}>
@@ -13,7 +16,9 @@ const Orders = () => {
         <span>amount ({pair.accountingLabel})</span>
       </div>
       <Buy />
-      <p className={styles.last}>9854.236</p>
+      <Typography textAlign={"end"} marginY={3} fontSize={28}>
+        {latestPrice} {pair.accountingLabel}
+      </Typography>
       <Sell />
     </div>
   );
