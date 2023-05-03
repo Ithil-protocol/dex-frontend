96258693fa2a85e86c1c0aff441652e1dc52954a
s-stalwart-s <stalwart.programmer@gmail.com>
2023-05-03 05:43:27 +0330

    useChangeOrderStatus(feat): implement

---
+++ src/hooks/poolWrite/useChangeOrderStatus.ts
@@ -0,0 +1,35 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { OpenOrderEvent, Status } from "types";
+
+export const useChangeOrderStatus = (
+  address: string,
+  poolAddress: string,
+  transactionHash: string
+) => {
+  const queryClient = useQueryClient();
+
+  return (status: Status) => {
+    queryClient.setQueryData<OpenOrderEvent[]>(
+      ["userOrderCreatedEvent", address, poolAddress],
+      (prev) => {
+        if (!prev) return;
+
+        const index = prev.findIndex(
+          (i) => i.transactionHash === transactionHash
+        );
+
+        if (index > -1) {
+          const order = { ...prev[index] };
+          const copyOrders = [...prev];
+          copyOrders.splice(index, 1, {
+            ...order,
+            status,
+          });
+          return copyOrders;
+        }
+
+        return prev;
+      }
+    );
+  };
+};
