a37b2e73c841cdfaa97950390c166d4c14a996b6
s-stalwart-s <stalwart.programmer@gmail.com>
2023-05-03 06:25:16 +0330

    useSellAmountConverter(fix): relocate

---
+++ src/hooks/converters/useSellAmountConverter.ts
@@ -0,0 +1,13 @@
+import { usePoolStore } from "store";
+import { BigNumber, utils } from "ethers";
+import { fixPrecision } from "utility/convertors";
+
+export const useSellAmountConverter = () => {
+  const sellPool = usePoolStore((store) => store.sellPool);
+  return (amount: BigNumber) => {
+    const value = Number(
+      utils.formatUnits(amount, sellPool.underlying.decimals)
+    );
+    return fixPrecision(value, sellPool.underlying.displayPrecision);
+  };
+};
