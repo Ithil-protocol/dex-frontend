833de15d1b5741aaa11f4204c57a1b18987b9ad7
s-stalwart-s <stalwart.programmer@gmail.com>
2023-05-06 00:29:04 +0330

    update boost group button style

--- src/components/CreateOrder/Inputs/Boost/GroupButton.tsx
+++ src/components/CreateOrder/Inputs/Boost/GroupButton.tsx
@@ -1,22 +1,27 @@
+import { BoostName } from "@/types";
 import { Button } from "@mui/material";
 
 interface Props {
   disabled: boolean;
-  groupButtonHandler: (item: number) => void;
+  groupButtonHandler: (item: BoostName) => void;
 }
 
 const BoostGroupButton: React.FC<Props> = ({
   disabled,
   groupButtonHandler,
 }) => {
+  const buttons: BoostName[] = ["no boost", "slow", "normal", "fast"];
+
   return (
     <div
       style={{
         display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         borderRadius: "0px 0px 5px 5px",
       }}
     >
-      {[25, 50, 75, 100].map((item, i) => {
+      {buttons.map((item, i) => {
         return (
           <Button
             variant="contained"
@@ -26,17 +31,23 @@ const BoostGroupButton: React.FC<Props> = ({
             disableElevation
             sx={(theme) => ({
               borderRadius: 0,
+              fontSize: 10,
               backgroundColor: theme.palette.background.default,
-              borderRight: `1px solid ${theme.palette.background.paper}`,
+              borderRight:
+                i < buttons.length - 1
+                  ? `1px solid ${theme.palette.background.paper}`
+                  : 0,
               "&:hover": {
                 backgroundColor: theme.palette.background.default,
               },
               padding: "5px",
               width: "100%",
+              borderEndEndRadius: i === buttons.length - 1 ? "5px" : 0,
+              borderEndStartRadius: i === 0 ? "5px" : 0,
             })}
             size="small"
           >
-            {item}%
+            {item}
           </Button>
         );
       })}
