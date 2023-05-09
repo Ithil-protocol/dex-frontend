d11eccac599572483815eea94363460b3fab2112
dev-aly3n <dev.aly3n@gmail.com>
2023-05-09 18:51:06 +0330

    add icons

--- icons.ts
+++ icons.ts
@@ -1,5 +1,7 @@
 import ETH from "cryptocurrency-icons/svg/icon/eth.svg";
 import USDC from "cryptocurrency-icons/svg/icon/usdc.svg";
+import DAI from "cryptocurrency-icons/svg/icon/dai.svg";
+import WBTC from "cryptocurrency-icons/svg/icon/wbtc.svg";
 
 // the "keys" in icons object must match underlyingLabels or accountingLabels
 // if underlyingLabel is TokenX we should add it in the object like this:
@@ -8,4 +10,6 @@ import USDC from "cryptocurrency-icons/svg/icon/usdc.svg";
 export const icons = {
   WETH: ETH,
   USDC,
+  DAI,
+  WBTC,
 };
