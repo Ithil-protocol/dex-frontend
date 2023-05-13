8000ac9a5feb7153dd7eb74bc98298b7a7e0cab6
dev-aly3n <dev.aly3n@gmail.com>
2023-05-13 03:54:02 +0330

    fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,7 +6,7 @@ export const STAKED_PRECISION = 6;
 //if max boost is zero or less than 0.001, then FAST option will be MIN_FAST_BOOST
 export const MIN_FAST_BOOST = 0.001;
 
-//this will multiply in maxBoost
+//this will multiply in maxBoost. if maxBoost is 0.015 and user clicks on FAST, then it will multiply by  INCREASE_MAX_BOOST_FACTOR
 export const INCREASE_MAX_BOOST_FACTOR = 1.01;
 
 export const CREATE_ORDER_DEADLINE = 60; //seconds
