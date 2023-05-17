5f9b3a27851122eb348007231f4336bd455f729c
dev-aly3n <dev.aly3n@gmail.com>
2023-05-17 15:22:34 +0330

    change provider

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -9,28 +9,11 @@ const blockpiBaseHttpUrl = "https://ethereum-sepolia.blockpi.network/v1/rpc/";
 const blockpiBaseWssUrl = "wss://ethereum-sepolia.blockpi.network/v1/ws/";
 
 export const allProviders: ChainProviderFn<typeof network>[] = [
-  jsonRpcProvider({
-    rpc: () => ({
-      http:
-        network.rpcUrls.alchemy.http +
-        "/" +
-        process.env.NEXT_PUBLIC_ALCHEMY_ID!,
-      webSocket:
-        network.rpcUrls.alchemy.webSocket +
-        "/" +
-        process.env.NEXT_PUBLIC_ALCHEMY_ID!,
-    }),
+  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID! }),
+  infuraProvider({
+    apiKey: process.env.NEXT_PUBLIC_INFURA_ID!,
   }),
-  jsonRpcProvider({
-    rpc: () => ({
-      http:
-        network.rpcUrls.infura.http + "/" + process.env.NEXT_PUBLIC_INFURA_ID!,
-      webSocket:
-        network.rpcUrls.infura.webSocket +
-        "/" +
-        process.env.NEXT_PUBLIC_INFURA_ID!,
-    }),
-  }),
+  publicProvider(),
   jsonRpcProvider({
     rpc: () => ({
       http: blockpiBaseHttpUrl + process.env.NEXT_PUBLIC_BLOCKPI_ID!,
