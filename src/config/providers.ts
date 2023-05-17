dd3a83a88bb6f972c6efe8165d273b01664aee74
dev-aly3n <dev.aly3n@gmail.com>
2023-05-17 15:18:33 +0330

    fix

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -5,25 +5,36 @@ import { publicProvider } from "wagmi/providers/public";
 import { network } from "./network.ts";
 import { ChainProviderFn } from "wagmi";
 
-const blockpiBaseUrl = "://ethereum-sepolia.blockpi.network/v1/rpc/";
+const blockpiBaseHttpUrl = "https://ethereum-sepolia.blockpi.network/v1/rpc/";
+const blockpiBaseWssUrl = "wss://ethereum-sepolia.blockpi.network/v1/ws/";
 
 export const allProviders: ChainProviderFn<typeof network>[] = [
-  // jsonRpcProvider({
-  //   rpc: () => ({
-  //     http: network.rpcUrls.alchemy.http + "/" + process.env.NEXT_PUBLIC_ALCHEMY_ID!,
-  //     webSocket: network.rpcUrls.alchemy.webSocket + "/" + process.env.NEXT_PUBLIC_ALCHEMY_ID!,
-  //   }),
-  // }),
-  // jsonRpcProvider({
-  //   rpc: () => ({
-  //     http: network.rpcUrls.infura.http + "/" + process.env.NEXT_PUBLIC_INFURA_ID!,
-  //     webSocket: network.rpcUrls.infura.webSocket + "/" + process.env.NEXT_PUBLIC_INFURA_ID!,
-  //   }),
-  // }),
   jsonRpcProvider({
     rpc: () => ({
-      http: "https" + blockpiBaseUrl + process.env.NEXT_PUBLIC_BLOCKPI_ID!,
-      webSocket: "wss" + blockpiBaseUrl + process.env.NEXT_PUBLIC_BLOCKPI_ID!,
+      http:
+        network.rpcUrls.alchemy.http +
+        "/" +
+        process.env.NEXT_PUBLIC_ALCHEMY_ID!,
+      webSocket:
+        network.rpcUrls.alchemy.webSocket +
+        "/" +
+        process.env.NEXT_PUBLIC_ALCHEMY_ID!,
+    }),
+  }),
+  jsonRpcProvider({
+    rpc: () => ({
+      http:
+        network.rpcUrls.infura.http + "/" + process.env.NEXT_PUBLIC_INFURA_ID!,
+      webSocket:
+        network.rpcUrls.infura.webSocket +
+        "/" +
+        process.env.NEXT_PUBLIC_INFURA_ID!,
+    }),
+  }),
+  jsonRpcProvider({
+    rpc: () => ({
+      http: blockpiBaseHttpUrl + process.env.NEXT_PUBLIC_BLOCKPI_ID!,
+      webSocket: blockpiBaseWssUrl + process.env.NEXT_PUBLIC_BLOCKPI_ID!,
     }),
   }),
 ];
