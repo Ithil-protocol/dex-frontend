42ccab446d598564fa45ce6fea2a3dcec065e3f9
OxMarco <>
2023-05-18 11:47:18 +0200

    fix chart

--- icons.ts
+++ icons.ts
@@ -1,7 +1,6 @@
 import ETH from "cryptocurrency-icons/svg/icon/eth.svg";
 import USDC from "cryptocurrency-icons/svg/icon/usdc.svg";
-import DAI from "cryptocurrency-icons/svg/icon/dai.svg";
-import WBTC from "cryptocurrency-icons/svg/icon/wbtc.svg";
+import USDT from "cryptocurrency-icons/svg/icon/usdt.svg";
 
 // the "keys" in icons object must match underlyingLabels or accountingLabels
 // if underlyingLabel is TokenX we should add it in the object like this:
@@ -10,6 +9,5 @@ import WBTC from "cryptocurrency-icons/svg/icon/wbtc.svg";
 export const icons = {
   WETH: ETH,
   USDC,
-  DAI,
-  WBTC,
+  USDT,
 };
