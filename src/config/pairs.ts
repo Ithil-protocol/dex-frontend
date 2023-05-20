788c61907cde75be37c3023ddc5b31b80431e82a
dev-aly3n <dev.aly3n@gmail.com>
2023-05-20 20:32:26 +0330

    add wethusdc pairs

--- src/config/pairs.ts
+++ src/config/pairs.ts
@@ -3,7 +3,7 @@ import { PairInitial } from "@/types";
 export const pairs: PairInitial[] = [
   {
     tick: 1,
-    undToAccBase: 10,
+    undToAccBase: 20000,
     accToUndBase: 20000,
     undToAccChartUrl: "UNISWAP3ETH:USDCUSDT",
     accToUndChartUrl: "UNISWAP3ETH:USDTUSDC",
@@ -16,6 +16,23 @@ export const pairs: PairInitial[] = [
       label: "USDT",
       address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
       displayPrecision: 6,
+    },
+  },
+  {
+    tick: 5,
+    undToAccBase: 10,
+    accToUndBase: 20000,
+    undToAccChartUrl: "UNISWAP3ARBITRUM:WETHUSDC",
+    accToUndChartUrl: "UNISWAP3ARBITRUM:USDCWETH",
+    underlying: {
+      label: "WETH",
+      address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+      displayPrecision: 6,
+    },
+    accounting: {
+      label: "USDC",
+      address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+      displayPrecision: 2,
     },
   },
 ];
