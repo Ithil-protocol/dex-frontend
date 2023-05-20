af4078d41ed675868d306e34d70b27991e00a976
dev-aly3n <dev.aly3n@gmail.com>
2023-05-20 04:47:21 +0330

    add a disclaimer

---
+++ src/components/Common/Disclaimer/index.tsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+
+const Disclaimer = () => (
+  <Typography>
+    This is unaudited software. By connecting your wallet, you agree to use this
+    app at your own risk.
+  </Typography>
+);
+
+export default Disclaimer;
